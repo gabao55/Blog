2ef100fd40e29a947c768813ce6f381a42737e1a
gabao55 <gabriel.s.rosin@gmail.com>
2021-09-03 13:43:46 -0300

    Modifying comments' model

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -5,11 +5,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Comments(models.Model):
-    comment_name = models.CharField(max_length=150)
-    comment_email = models.EmailField()
-    comment = models.TextField()
+    comment_name = models.CharField(max_length=150, verbose_name='Name*:')
+    comment_email = models.EmailField(verbose_name='E-mail*:')
+    comment = models.TextField(verbose_name='Comment*:')
     post_comment = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    comment_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
     comment_date = models.DateTimeField(default=timezone.now)
     comment_published = models.BooleanField(default=False)
 
